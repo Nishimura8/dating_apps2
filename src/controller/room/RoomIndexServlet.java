eaf1e34ffeebe53d642d572c38c6f77adbd5be75
Nishimura8 <51505801+Nishimura8@users.noreply.github.com>
2024-09-18 16:40:10 +0900

    マッチング数0だとエラー落ちしてしまう不具合の解消

--- src/controller/room/RoomIndexServlet.java
+++ src/controller/room/RoomIndexServlet.java
@@ -54,20 +54,25 @@ public class RoomIndexServlet extends HttpServlet {
             }
         }
 
-        List<Follow> follows = em.createNamedQuery("checkMyRoom", Follow.class)
-                                     .setParameter("follow", login_user)
-                                     .setParameter("list", id_list)
-                                     .getResultList();
-
+        List<Follow> follows = null;
         List<Follow> followCheck = new ArrayList<Follow>();
-        List<Integer> roomSave = new ArrayList<Integer>();
-        for (Follow follow : follows) {
-            int room =  follow.getRoom().getId();
-            if(!roomSave.contains(room)) {
-                followCheck.add(follow);
-                roomSave.add(room);
+        if(id_list.size() != 0) {
+             follows = em.createNamedQuery("checkMyRoom", Follow.class)
+                                         .setParameter("follow", login_user)
+                                         .setParameter("list", id_list)
+                                         .setFirstResult(16 * (page - 1))
+                                         .setMaxResults(16)
+                                         .getResultList();
+
+            List<Integer> roomSave = new ArrayList<Integer>();
+            for (Follow follow : follows) {
+                int room =  follow.getRoom().getId();
+                if(!roomSave.contains(room)) {
+                    followCheck.add(follow);
+                    roomSave.add(room);
+                }
             }
-    }
+        }
 
         List<User> users = em.createNamedQuery("getAllUsers", User.class)
                 .setFirstResult(16 * (page - 1))
